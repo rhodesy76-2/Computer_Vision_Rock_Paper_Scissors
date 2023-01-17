e4c5ab8d720e4d7a9f9c06d219a73859f9a9e34b
rhodesy76-2 <113443029+rhodesy76-2@users.noreply.github.com>
2023-01-17 20:04:38 +0100

    Saving practicles progress -  Import

--- practicals.py
+++ practicals.py
@@ -163,3 +163,132 @@ names = {"tom": "Tomatoes",
 budget = 10.00
 running_total = 0
 receipt = []
+
+while budget >= 0:
+    for item, cost, amount in order_list:
+        budget = budget - (cost * amount)
+        if budget <= 0:
+            print(f"You have exceeded your budget by {-budget}, by buying {amount} {names[item]}")
+            break
+        running_total = running_total + (cost * amount)
+        receipt.append(f"Item Code: {names[item]}, Amount: {amount}, Individaul Price: £{cost}, Total cost: £{cost*amount}, Running Total: {running_total}")
+        print(receipt)
+    
+
+# %%
+#Create a list comprehension that squares even arguments, and squares odd arguments after adding one to them
+#Test it on the following list my_list = [1, 5, 8, 6, 21]
+#The output should be:
+#[4, 36, 64, 36, 484]
+my_list = [1, 5, 8, 6, 21]
+new_list = []
+
+for i in my_list:
+    if i % 2 == 0:
+        i= i**2
+        new_list.append(i)
+    else:
+       i = (i+1)**2
+       new_list.append(i)
+print(new_list)
+# %%
+# Dictionairies and List Comprehension
+# Create two dictionaries, each one with 2 keys
+# name: a string of your name
+# skills: a list of strings
+#Put both of them in a list
+#Now index that list of dictionaries to find the last letter of the first skill of the last dictionary
+#Create a list comprehension which maps that list to a list of the length of names
+#Add that list together to get the total number of characters in all of the names
+#WTF?
+my_list = [{'name':'James', "skills":["Python", "Java"]}, {"name": "Rhodes", "skills": ["Lisp", "Prolog"]}]
+
+my_list[1]
+#%%
+my_list[1]["skills"]
+#%%
+my_list[1]["skills"][0][-1]
+#%%
+my_list = [{'name':'James', "skills":["Python", "Java"]}, {"name": "Rhodes", "skills": ["Lisp", "Prolog"]}]
+my_list[0]['name']
+len(my_list[0]['name'])
+
+#%%
+name_length = []
+
+for i in range(0,2):
+    name_length.append(len(my_list[i]['name']))
+print(name_length)
+print(sum(name_length))
+
+
+#%%
+# Shop Item Filter
+# Filter shop_dict using list comprehension to find only items with values of over 1.00
+#Assign them to a list called filtered_shop by their full names, not their codes, using names_dict.
+#shop_dict = {"tom":0.87, "sug":1.09, "ws":0.29, "cc":1.89, "ccz":1.29}
+# names_dict = {"tom":"Tomatoes", "sug":"Sugar", "ws":"Washing Sponges", "cc":"Coca-Cola", "ccz":"Coca-Cola Zero"}
+
+filtered_shop =[]
+shop_dict = {"tom":0.87, "sug":1.09, "ws":0.29, "cc":1.89, "ccz":1.29}
+names_dict = {"tom":"Tomatoes", "sug":"Sugar", "ws":"Washing Sponges", "cc":"Coca-Cola", "ccz":"Coca-Cola Zero"}
+
+
+
+for key, value in shop_dict.items():
+    if value > 1.00:
+        filtered_shop.append(names_dict[key])
+print(filtered_shop)
+    
+
+
+# %%
+# Write a program to produce the following pattern.
+# Here's a clue: Use nested for loops, using one nested loop to increase in size and another to decrease.*
+#Start with n=5
+#Example:
+#?
+#? ?
+#? ? ?
+#? ? ? ?
+#? ? ? ? ?
+#? ? ? ?
+#? ? ?
+#? ?
+#?
+n = 5
+for i in range(n+1):
+    for j in range(i):
+        print("?",end=" ")
+    print('')
+for i in range(n,0,-1):
+    for j in range(i):
+        print('?', end=" ")
+    print('')
+  
+    
+# %%
+#Write a program to check whether each number from 10 to 50 is prime
+#Your answer should take the following format:
+#"x is a prime number." for primes
+#"x is not a prime number because y is a factor of x." for non-primes
+#Here's a clue: This is possible using a while loop nested in a for loop or using a nested for loop; if you can, try to find both
+
+for num in range (10, 51):
+    count = 0
+    for i in range(2, (num//2 + 1)):
+        if(num % i == 0):
+            print(num,"is not a prime number")
+            count = count + 1
+            print(i,"times",num//i,"is",num)
+            break
+
+    if (count == 0 and num != 1):
+        print(num,"is a prime number")
+
+# %%
+#10 imports
+
+import sys
+sys.path
+# %%
